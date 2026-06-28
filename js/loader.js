d6588110de765597c27681b0bae1ab37f5afeee8
chudarraya <chuprynina.darya@gmail.com>
2026-06-29 04:02:51 +1000

    Update loader.js

--- js/loader.js
+++ js/loader.js
@@ -1,5 +1,5 @@
 import * as THREE from "https://unpkg.com/three@0.160.0/build/three.module.js";
-import { OrbitControls } from "https://unpkg.com/three@0.160.0/examples/jsm/controls/OrbitControls.js";
+import { CONFIG } from "./config.js";
 
 /**
  * Загрузчик текстур с пакетной обработкой и прогрессом
