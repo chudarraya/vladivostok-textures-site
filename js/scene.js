7641b3c17b4d1c19bbee2d9ffb67492f25301fb7
chudarraya <chuprynina.darya@gmail.com>
2026-06-29 04:03:06 +1000

    Update scene.js

--- js/scene.js
+++ js/scene.js
@@ -1,6 +1,6 @@
 import * as THREE from "https://unpkg.com/three@0.160.0/build/three.module.js";
 import { OrbitControls } from "https://unpkg.com/three@0.160.0/examples/jsm/controls/OrbitControls.js";
-import { CONFIG } from './config.js';
+import { CONFIG } from "./config.js";
 
 /**
  * Менеджер Three.js сцены
